ea1381c5208b2a003ceb4b8b2ff2663a809e2e2b
Tev'n Powers <tevnpowers@gmail.com>
2026-03-05 21:36:53 -0500

    primarily updating navLayout. init check-in of site style.

--- client/app/routes/navLayout.tsx
+++ client/app/routes/navLayout.tsx
@@ -1,69 +1,67 @@
 import { useState } from 'react';
 import { NavLink, Outlet } from 'react-router';
+import { MenuIcon } from '~/components/icons/menu';
 import { navRoutes } from '~/routes';
 
 export default function Layout() {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <div
-      style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}
-    >
-      <NavBar toggleOpen={() => setIsOpen(!isOpen)} />
-      <div style={{ display: 'flex', flexDirection: 'row', flex: 1 }}>
-        {isOpen ? <NavMenu /> : null}
-        <div style={{ flex: 1 }}>
-          <Outlet />
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className='page-container'>
+			<div className='content-container'>
+				<NavBar toggleOpen={() => setIsOpen(!isOpen)} />
+				<div className='outlet-container'>
+					<div style={{ flex: 1 }}>
+						<Outlet />
+					</div>
+					{isOpen ? <NavMenu /> : null}
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 const NavMenu = () => {
-  return (
-    <nav
-      style={{
-        padding: '36px',
-        backgroundColor: '#666',
-      }}
-    >
-      <ul
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <NavLink style={{ marginBottom: '12px' }} to={navRoutes.home()}>
-          Home
-        </NavLink>
-        <NavLink style={{ marginBottom: '12px' }} to={navRoutes.modules.home()}>
-          Modules
-        </NavLink>
-        <NavLink
-          style={{ marginBottom: '12px' }}
-          to={navRoutes.resources.home()}
-        >
-          Resources
-        </NavLink>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav className='nav-menu'>
+			<ul>
+				<NavLink to={navRoutes.home()}>Home</NavLink>
+				<NavLink to={navRoutes.modules.home()}>Modules</NavLink>
+				<NavLink to={navRoutes.resources.home()}>Resources</NavLink>
+			</ul>
+		</nav>
+	);
 };
 
 const NavBar = ({ toggleOpen }: { toggleOpen: () => void }) => {
-  return (
-    <div style={{ display: 'flex' }}>
-      <div
-        style={{ width: '60px', height: '60px', backgroundColor: '#ccc' }}
-        onClick={toggleOpen}
-      ></div>
-      <div style={{ flex: 1, backgroundColor: '#999' }}></div>
-    </div>
-  );
+	return (
+		<div className='space-flex-horizontal'>
+			<NavLink className='brand-mark' to={navRoutes.home()}>Real Imaginary</NavLink>
+			<button className='button-svg-icon' onClick={toggleOpen}>
+				<MenuIcon info={{ width: 45, height: 45, color: '#E14A4A'}}/>
+			</button>
+		</div>
+	);
 };
 
 const Footer = () => {
-  return <div style={{ height: '120px', backgroundColor: '#333' }}></div>;
+	return (
+		<div className="footer-container">
+			<div className="footer-content">
+				<h1>Digital<br/>Commons<br/>Collective</h1>
+				<div className="links">
+					<NavLink to={navRoutes.home()}>Home</NavLink>
+					<NavLink to={navRoutes.modules.home()}>Modules</NavLink>
+					<NavLink to={navRoutes.resources.home()}>Resources</NavLink>
+				</div>
+				<div className="subscribe-container">
+					<form>
+						<input className="email-input" type="text" name="email" placeholder="Enter your email"/>
+						<input className="subscribe-button" type="submit" value="Subscribe"/>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
